54df0b7af62db4a6b43263993884d410865f1bb9
ndimit02 <secondslearnin@gmail.com>
2025-03-12 05:50:55 +0500

    confidenece

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -133,7 +133,7 @@ const Navbar = () => {
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
             className="w-full px-10 py-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-[#53CFFF] transition-all duration-300 rounded-full"
-            style={{ borderRadius: "0px 40px 0px 40px" }}
+            // style={{ borderRadius: "0px 40px 0px 40px" }}
           />
           <button
             type="submit"
